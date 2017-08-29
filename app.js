1b16b2cc9b25407f4633d4b4b49ca9dcb9c55efe
Marty Kunsman <kunsmanmartin@gmail.com>
2017-08-29 16:51:06 +0000

    Finsished taking course today.

--- app.js
+++ app.js
@@ -14,6 +14,20 @@ var port = process.env.PORT;
 app.use(logger('dev'));
 app.use(jsonParser());
 
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/qa');
+
+var db = mongoose.connection;
+
+db.on('error', function(err){
+    console.error('connection error:', err);
+});
+
+db.once('open', function(){
+    console.log('db connection successful');
+});
+
 app.use('/questions', routes)
 
 //catch 404 and forward to error handler
