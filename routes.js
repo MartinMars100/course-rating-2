de961a7ca52ba4c7d6cd6cc5283898d6d844562f
Marty Kunsman <kunsmanmartin@gmail.com>
2017-09-20 18:30:04 +0000

    Commit after getting db seeded.

--- routes.js
+++ routes.js
@@ -2,25 +2,6 @@ var express = require('express'),
     router = express.Router(),
     User = require('./models').User,
     mid = require('./middleware/auth');
-    // Loan = require("../models").Loan,
-    // Patron = require("../models").Patron,
-    // moment = require('moment');
-
-// GET /api/users 200 
-// Return the current authenticated user 
-// router.get('/', mid.authenticate, function(req, res, next) { 
-    // router.get('/', function(req, res, next) { 
-    //   res.json({response: "You sent me a GET request"}) ;
-//     console.log('log get user fired');
-//     res.send({
-// 		        _id: req.user._id,
-// 		        fullName: req.user.fullName
-// 	});
-	  // get user from req and send as json object 
-    // res.json({
-    //   data: [req.user]
-    // });
-// });
 
 router.get('/', function(req, res, next){
     console.log('log get all users route');
@@ -29,7 +10,6 @@ router.get('/', function(req, res, next){
                if(err) return next(err);
                res.json(users);
              });
-
 });
 
 // POST /api/users 201
@@ -48,43 +28,14 @@ router.post('/', function(req, res, next) {
           return next(err);
         }
           
-        //   create object with form input
-        var userData = {
-          fullName: req.body.fullName,  
-          email: req.body.emailAddress,
-          password: req.body.password,
-          confirmPassword: req.body.confirmPassword
-        };
-          
-        console.log('log almost to create User');
-        console.log('log req.body.fullName = ' + req.body.fullName);
-        
-        // return res.status(201)
-        // .location('/')
-        // .end();
-        
         // var user = new models.User(req.body);
         var user = new User(req.body);
-        user.save(userData,function(err) {
-          if (err) return next(err)
+        user.save(function(err) {
+          if (err) return next(err);
             res.status(201);
             // res.location('/');
             res.end();
         });
-        
-        //   use schema's create method to insert documet into Mongo
-      //   User.save(userData, function(error) {
-      //     console.log('Inserting into MongoDb');
-			   // if (err) {
-			   //   console.log('error creating User');
-				  //   return next(err);
-		    //   } else {
-				  //     return res.status(201)
-					 //   .location('/')
-					 //   .end();	
-			   // }
-      //   });
-          
       } else {
         console.log('log All fields are not entered');
         err = new Error('All fields required');
